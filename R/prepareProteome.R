2ab427f8c53685ad1c2ffc5491f93eeaa52e5d08
Jianhong Ou <jianhongou@Jianhongs-MacBook-Pro.local>
2019-05-02 16:56:17 -0400

    add  new function of prepareProteomeByFTP and prepareProteomeByUniProtWS

--- R/prepareProteome.R
+++ R/prepareProteome.R
@@ -1,8 +1,9 @@
 #' prepare proteome for background building
 #' @description prepare proteome from UniProt webserver or a fasta file
-#' @param UniProt.ws an object of UniProt.ws
+#' @param source An object of \code{\link{UniProt.ws}} or A character "UniProt".
 #' @param fasta fasta file name or an object of AAStringSet
 #' @param species an character to assign the species of the proteome
+#' @param ... parameters could be passed to \link{prepareProteomeByFTP}.
 #' @import Biostrings
 #' @export
 #' @return an object of Proteome which contain protein sequence information.
@@ -17,33 +18,17 @@
 #'  }
 #' @keywords misc
 
-prepareProteome <- function(UniProt.ws, fasta, species="unknown"){
-    if(!missing(UniProt.ws) && class(UniProt.ws)=="UniProt.ws"){
-        egs <- keys(UniProt.ws, "ENTREZ_GENE")
-        cols <- c("SEQUENCE", "ID")
-        proteome <- select(UniProt.ws, egs, cols, "ENTREZ_GENE")
-        proteome$SEQUENCE <- gsub(" ", "", proteome$SEQUENCE, fixed=TRUE)
-        proteome$LEN <- nchar(proteome$SEQUENCE)
-        return(new("Proteome",
-                   proteome=proteome,
-                   type="UniProt",
-                   species=species))
-    } else {
-        if(!missing(fasta)){
-            if(length(fasta)==1 && class(fasta)=="character"){
-                fasta <- readAAStringSet(fasta)
-            }
-            if(class(fasta)!="AAStringSet"){
-                stop("fasta should be character or an object AAStringSet", call.=FALSE)
-            }
-            proteome <- data.frame(SEQUENCE=as.character(fasta),
-                                   ID=names(fasta),
-                                   stringsAsFactors=FALSE)
-        }
-        return(new("Proteome", 
-                   proteome=proteome,
-                   type="fasta",
-                   species=species))
+prepareProteome <- function(source, fasta, species="unknown", ...){
+  if(!missing(source)){
+    if(is(source, "UniProt.ws")){
+      return(prepareProteomeByUniProtWS(UniProt.ws=source, species=species))
+    }else{
+      return(prepareProteomeByFTP(source = "UniProt", species = species, ...))
     }
-    stop("Please check you inputs.", call.=FALSE)
+  }else{
+    if(!missing(fasta)){
+      return(prepareProteomeByUniProtWS(fasta=fasta, species=species))
+    }
+  }
+  stop("Please check you inputs.", call.=FALSE)
 }
